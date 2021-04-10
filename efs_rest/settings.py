4b8e85330d99ab6b44bc16a9a32bb152525fc51a
sandmish <54651939+sandmish@users.noreply.github.com>
2021-04-10 18:35:04 -0500

    settings

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -81,21 +81,21 @@ WSGI_APPLICATION = 'efs_rest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-#
 # DATABASES = {
-#      'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'EagleFinancialServices',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Pa$$wrd1'
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+#
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EagleFinancialServices',
+        'USER': 'postgres',
+        'PASSWORD': 'Pa$$wrd1'
+    }
+}
 
 
 
